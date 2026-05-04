dce43de717f4cfbae1e62a879f4bf69d84957ff5
codegravityllc <85649738+codegravityllc@users.noreply.github.com>
2026-05-04 10:20:32 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // This matches any URL starting with /api
+      '/api': {
+        target: 'http://54.91.197.157:8080',
+        changeOrigin: true,
+        secure: false, // Prevents issues if the target has no SSL
+      },
+    },
+  },
 })
